6aced651bcd2b46c1d50852fabd59c2b2227c7f8
Wesley Ferrell <ferrellw@gmail.com>
2018-05-09 00:09:00 -0700

    Back to port 5000. Pi friendly template.

--- app.py
+++ app.py
@@ -33,6 +33,30 @@ def index():
     return render_template('index.html',outlets=outlets)
 
 
+#Main page.
+@app.route('/pi/')
+def pi():
+    ##Lets get the status for the outlets.
+    #Define outlets. This list should match what is in power.py.
+    outlets = '[{"name": "Outlet Group 1","pin": "13","status": "","feed": "","form": "og1"},{"name": "Outlet Group 2","pin": "19","status": "","feed": "","form": "og2"},{"name": "Outlet Group 3","pin": "26","status": "","feed": "","form": "og3"},{"name": "Outlet Group 4","pin": "21","status": "","feed": "","form": "og4"},{"name": "Outlet Group 5","pin": "20","status": "","feed": "yes","form": "og5"},{"name": "Outlet Group 6","pin": "16","status": "","feed": "yes","form": "og6"}]'
+    outlets = json.loads(outlets)
+    #Loop over each item in the list.
+    for outlet in outlets:
+        #Get the status of the GPIO pin for each outlet.
+        ps = subprocess.Popen(('gpio', '-g', 'read', outlet['pin']), stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
+        output = str(ps.communicate())
+        #Output is not boolean. Lets fix that.
+        output = output.translate(None,"\n',N('on\e) ")
+        #Update the list to reflect status of outlet.
+        if output == "1":
+            outlet['status'] = "Off"
+            outlet['selected'] = "selected"
+        if output == "0":
+            outlet['status'] = "On"
+        #Now that we have the status of the outlets, present to the template for rendering.
+    return render_template('pi.html',outlets=outlets)
+
+
 # @app.route('/status/', methods=['POST'])
 # def status():
 #     cmd = ["python",scripts+"/power.py", "status"]
@@ -72,4 +96,4 @@ def ogcontrol():
     return redirect("/", code=302)
 
 if __name__ == "__main__":
-    	app.run(host='0.0.0.0',port=80,debug=False)
+    	app.run(host='0.0.0.0',port=5000,debug=False)
